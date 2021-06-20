e729b231dfbec5e50a8d1a3ad10c866dfc465ee0
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-06-20 13:35:16 +0700

    Option DB

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(
-	`mongodb://dimas:${process.env.PASS}@cluster0-shard-00-00.t2x83.mongodb.net:27017,cluster0-shard-00-01.t2x83.mongodb.net:27017,cluster0-shard-00-02.t2x83.mongodb.net:27017/quote-api?ssl=true&replicaSet=atlas-rs5qbf-shard-0&authSource=admin&retryWrites=true&w=majority`,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: true
-	},
-	(err) => {
-		if (err) return console.log(err);
-		console.log("Mongoose Connected");
-	}
-);
+module.exports = function initConnection(callback) {
+	mongoose.connect(
+		`mongodb://dimas:${process.env.PASS}@cluster0-shard-00-00.t2x83.mongodb.net:27017,cluster0-shard-00-01.t2x83.mongodb.net:27017,cluster0-shard-00-02.t2x83.mongodb.net:27017/quote-api?ssl=true&replicaSet=atlas-rs5qbf-shard-0&authSource=admin&retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: true
+		}
+	);
+	const db = mongoose.connection;
+	db.on("error", function (err) {
+		console.error(err);
+		process.exit(1);
+	});
+	db.once("open", function () {
+		console.log("Connected to database");
+		callback();
+	});
+};
--- src/main.js
+++ src/main.js
@@ -5,67 +5,72 @@ const bcrypt = require("bcrypt");
 const rateLimit = require("express-rate-limit");
 
 // Connect Mongoose
-require("./db/mongoose");
-
-// Configurations
-app.use(express.json());
-const port = process.env.PORT || 3000;
-require("dotenv").config();
-
-// Handle Delete Method Permission
-app.use(async (req, res, next) => {
-	if (req.method === "POST") {
-		const { authorization } = req.headers;
-
-		if (!authorization) return res.status(401).send({ message: "Authorization Required" });
-
-		const isAuthorized = await bcrypt.compare(process.env.B64PASS, authorization.split(" ")[1]);
-		if (!isAuthorized) return res.status(401).send({ message: "Wrong Auth Token" });
-
-		return next();
-	}
-
-	if (req.method === "DELETE") {
-		if (!req.query.admin)
-			return res.status(401).send({
-				message: "DELETE Method only available for admin",
-				status: 401
-			});
-
-		if (req.query.admin !== process.env.ADMIN_PASS)
-			return res.status(401).send({
-				message: "Wrong Auth Token",
-				status: 401
-			});
-
-		return next();
-	}
-
-	next();
+const initConnection = require("./db/mongoose");
+initConnection(function () {
+	launchServer();
 });
 
-// Handle Maintenance
-// app.use((req, res, next) => {
-// 	res.status(503).send({ message: "Server Under Maintenance" });
-// });
+function launchServer() {
+	// Configurations
+	app.use(express.json());
+	const port = process.env.PORT || 3000;
+	require("dotenv").config();
 
-app.set("trust proxy", 1);
-app.use(
-	"/api/post/quote",
-	rateLimit({
-		windowMs: 24 * 60 * 60 * 1000, // 24 Hours
-		max: 5,
-		message: "Too many request from this device, please try again later"
-	})
-);
+	// Handle Delete Method Permission
+	app.use(async (req, res, next) => {
+		if (req.method === "POST") {
+			const { authorization } = req.headers;
 
-app.use("/api/post", require("./routes/newQuote")); // New Quote
-app.use("/api", require("./routes/getAllQuote")); // Get All Quote
-app.use("/api", require("./routes/deleteQuote")); // Delete Quote
+			if (!authorization) return res.status(401).send({ message: "Authorization Required" });
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(`${__dirname}/public/`));
-	app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+			const isAuthorized = await bcrypt.compare(process.env.B64PASS, authorization.split(" ")[1]);
+			if (!isAuthorized) return res.status(401).send({ message: "Wrong Auth Token" });
+
+			return next();
+		}
+
+		if (req.method === "DELETE") {
+			if (!req.query.admin)
+				return res.status(401).send({
+					message: "DELETE Method only available for admin",
+					status: 401
+				});
+
+			if (req.query.admin !== process.env.ADMIN_PASS)
+				return res.status(401).send({
+					message: "Wrong Auth Token",
+					status: 401
+				});
+
+			return next();
+		}
+
+		next();
+	});
+
+	// Handle Maintenance
+	// app.use((req, res, next) => {
+	// 	res.status(503).send({ message: "Server Under Maintenance" });
+	// });
+
+	app.set("trust proxy", 1);
+	app.use(
+		"/api/post/quote",
+		rateLimit({
+			windowMs: 24 * 60 * 60 * 1000, // 24 Hours
+			max: 5,
+			message: "Too many request from this device, please try again later"
+		})
+	);
+
+	app.use("/api/post", require("./routes/newQuote")); // New Quote
+	app.use("/api", require("./routes/getAllQuote")); // Get All Quote
+	app.use("/api", require("./routes/deleteQuote")); // Delete Quote
+
+	if (process.env.NODE_ENV === "production") {
+		app.use(express.static(`${__dirname}/public/`));
+		app.get(/.*/, (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+	}
+
+	app.listen(port, () => console.log(`Up on http://localhost:${port}`));
 }
-
-app.listen(port, () => console.log(`Up on http://localhost:${port}`));
